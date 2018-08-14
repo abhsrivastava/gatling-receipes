94caeba4f80730915a8595fbd6575524fb5b6bc7
Srivastava <Abhishek.Srivastava2@emc.com>
2018-08-14 11:43:17 -0500

    Initial commit

---
+++ user-files/simulations/computerdatabase/ElasticSimulation.scala
@@ -0,0 +1,59 @@
+package computerdatabase
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import scala.util.Random
+
+class ElasticSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:9200") 
+
+  val headers = Map("Content-Type" -> "application/json")
+  val randomData = Iterator.continually(
+    Map(
+        "RandomName" -> Random.alphanumeric.take(8).mkString,
+        "RandomAge" -> Random.nextInt(100)
+    )
+  )
+
+  val scn = scenario("LoadAndQuery")
+    .feed(randomData)
+    .exec(
+        http("ingest")
+            .post("/test/mytype")
+            .headers(headers)
+            .body(StringBody("""{"name": "${RandomName}", "age": ${RandomAge}}""")).asJSON
+    )
+    .pause(5)
+    .exec(
+        http("query")
+        .get("/test/_search?pretty&scroll=1m")
+        .headers(headers)
+        .body(StringBody("""{"size":1000}"""))
+        .check(status.is(200), jsonPath("$..took").ofType[Int].lessThan(50))
+    )
+
+  before {
+    scenario("SetupIndex")
+        .exec(
+            http("deleteindex")
+                .delete("/test")
+        )
+        .pause(5)
+        .exec(
+            http("createindex")
+                .put("/test")
+        )
+        .inject(atOnceUsers(1))
+  }  
+
+  setUp(
+      scn
+        .inject(
+            constantUsersPerSec(10) during (60 seconds) randomized
+        )
+        .protocols(httpConf)
+  )
+}
